41a7eca3eb7bcc2179b4f77e18b7fdf419dd0802
Ashley Clarke <crashley92@gmail.com>
2020-08-21 14:51:52 -0700

    review on strings

---
+++ README.md
@@ -0,0 +1,5 @@
+compiles it
+cl {name_of_file}.cpp
+
+run it
+{name_of_file}.exe
---
+++ strings/string.cpp
@@ -0,0 +1,14 @@
+//  pre-proccessor directive 
+ #include <iostream>
+
+ using namespace std;
+
+ int main() 
+ {
+     cout << "Enter number" << endl;
+     int num1;
+     cin >> num1;
+     cout << num1 << endl;
+     
+     return 0;
+ }
